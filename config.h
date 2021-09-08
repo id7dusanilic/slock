4029d2fb2cd0929b81dedbfb61f664ca8df646ba
Dusan Ilic <idtronicsrs@gmail.com>
2021-09-08 20:24:35 +0200

    Switched to config.h

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "minimalist";
-static const char *group = "minimalist";
+static const char *user  = "dusan";
+static const char *group = "dusan";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
